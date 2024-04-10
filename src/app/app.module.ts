065b04fa03eeb1f93399cefed1c956d96ea86a43
Eduardo <ecmartinez@teralco.com>
2024-04-10 17:30:29 +0200

    Sección 14 HeroesApp 2 Angular Material 188 a 199

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
     SharedModule,
   ],
